3f05ca07477c77bcd899e6a61c0a191f44170f01
Julia Almeida <juliadealmeidaalmeida@gmail.com>
2023-08-01 12:18:44 -0300

    01/08 aula

--- src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
@@ -1,6 +1,11 @@
 package com.itb.inf2dm.comercio.controller;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,27 +14,37 @@ import com.itb.inf2dm.comercio.model.Produto;
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
-	
+	List<Produto>listaDeProdutos = new ArrayList<Produto>();
 	
 	@GetMapping("/listar")
 	 
-	public String listaProdutos() {
+	public String listaProdutos(Model model) {
 		
 		Produto p1 = new Produto();
-		p1.setId (201);
-		p1.setnome ("maquina de lavar brastemp 12 litros");
-		p1.setcodigoBarra("16546416496151646");
-		p1.setpreco(3654.12);
+		p1.setId (20l);
+		p1.setDescricao("maquina de lavar ");
+		p1.setNome ("maquina de lavar brastemp 12 litros");
+		p1.setCodigoBarra("16546416496151646");
+		p1.setPreco(3654.12);
+		p1.setCodStatus(true);
+		
+		Produto p2= new Produto();
+		p2.setId (20l);
+		p2.setDescricao("microondas eletrolux");
+		p2.setNome ("Microondas ");
+		p2.setCodigoBarra("855223697412");
+		p2.setPreco(40000);
+		p2.setCodStatus(true);
+		
 		 listaDeProdutos.add(p1);
+		 listaDeProdutos.add(p2);
+		 model.addAttribute("listaDeProdutos",listaDeProdutos);
 		 return "produtos";
-		
-		
-	
-	}
+		}
 	@GetMapping("/novo-prod")
 	public String NovoProduto() {
 	return "novo-prod";
 	}
-	
 
+ 
 }
--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -1,48 +1,58 @@
 package com.itb.inf2dm.comercio.model;
 
 public class Produto {
-	private long id;
+	private long Id;
 	private String descricao;
 	private String nome;
 	private String codigoBarra;
 	private double preco;
+	private boolean CodStatus;
 	// public: modificador de aceeso livre para todas as classes
 	// private : modificador de acesso restrito apenas para membros da 
 	// propria classe entende-se por "membros (atributos e/oumétodos)
-	 
 	// métodos Setter´s : Adicionar e recuperar  dados  do atributo "respectivamente"
-	
-	public void setId(long Id) {
-		this.id = id;
+
+	public long getId() {
+		return Id;
 	}
-	public Long getId() {
-		return id;
+	public void setId(long id) {
+		Id = id;
 	}
-	
-	public void setdescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public String getdescricao() {
+	public String getDescricao() {
 		return descricao;
 	}
-	public void setnome(String nome) {
-		this.nome= nome;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
-	public String getnome() {
+	public String getNome() {
 		return nome;
 	}
-	public void setcodigoBarra(String codigoBarra) {
-		this.codigoBarra=codigoBarra;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
-	public String getcodigoBarra() {
+	public String getCodigoBarra() {
 		return codigoBarra;
 	}
-	public void setpreco(double preco) {
+	public void setCodigoBarra(String codigoBarra) {
+		this.codigoBarra = codigoBarra;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
 		this.preco = preco;
-		}
-	 public double getpreco() {
-		 return preco;
-	 }
+	}
+	public boolean isCodStatus() {
+		return CodStatus;
+	}
+	public void setCodStatus(boolean codStatus) {
+		CodStatus = codStatus;
+	}
+	
+	 
+	
+	
+	
 	
 	
 	
